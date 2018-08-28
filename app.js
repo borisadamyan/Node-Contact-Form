88f7b6ac88ffceda009eae320e745c0781706355
Boris Adamyan <boris.adamyan@gmail.com>
2018-08-28 16:51:01 +0400

    uploading photo attache

--- app.js
+++ app.js
@@ -3,11 +3,32 @@
  const exphbs = require('express-handlebars');
  const  path = require('path');
  const  nodemailer = require('nodemailer');
-
  const app = express();
-
- // VIEW
-
+ var multer  = require('multer');
+ var upload = multer({ dest: 'public/img' });
+  const multConf = {
+    storage: multer.diskStorage({
+      destination: function (req, file, next) {
+        next(null, 'public/img')
+      },
+      filename: function (req, file, next) {
+        console.log(file);
+        const  ext = file.mimetype.split('/')[1];
+        next(null, file.filename + '-' + Date.now() + '.' + ext);
+      }
+    }),
+    fileFilter: function (req, file, next) {
+      if(!file){
+        next();
+      }
+      const  image = file.mimetype.startsWith('image/');
+      if(image){
+        next(null, true)
+      }else{
+        next({message: "File type not supported"}, false)
+      }
+    }
+  };
  app.engine('handlebars', exphbs());
  app.set('view engine', 'handlebars');
 
@@ -24,7 +45,12 @@
    res.render('contact')
  });
 
- app.post('/send', (req, res) => {
+ app.post('/send', multer(multConf).single('photo'), (req, res) => {
+
+   if(req.file){
+     req.body.photo = req.file.filename;
+   }
+   console.log(req.body.photo);
    const output = `
       <p>You have a new contact request</p>
       <h3>Contact Details</h3>
@@ -35,16 +61,16 @@
         <li>Phone: ${req.body.phone} </li>
       </ul>
       <h3>Message</h3>
-      <p>${req.body.message}</p>
-   `;
+      <p>${req.body.message}</p>`;
    // create reusable transporter object using the default SMTP transport
+
    let transporter = nodemailer.createTransport({
      host: 'smtp.gmail.com',
      port: 587,
      secure: false, // true for 465, false for other ports
      auth: {
-       user: '*****', // generated ethereal user
-       pass:  '*****'// generated ethereal password
+       user: 'spy.armenia@gmail.com', // generated ethereal user
+       pass:  '55446606b'// generated ethereal password
      },
      tls:{
        rejectUnauthorized: false
@@ -57,7 +83,12 @@
      to: 'borammos@gmail.com', // list of receivers
      subject: 'Hello ✔ Node contact', // Subject line
      text: 'Hello world?', // plain text body
-     html: output // html body
+     html: output, // html body
+     attachments: [
+       {
+         path: __dirname +'/public/img/'+ req.body.photo
+       }
+     ]
    };
 
    // send mail with defined transport object
